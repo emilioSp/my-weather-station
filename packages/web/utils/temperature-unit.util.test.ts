f0c550f888ceb288ad9d2672d8fcc7e7641f9a44
Emilio Spatola <emiliosp.me@gmail.com>
2026-09-15 21:32:35 +0200

    chore: add vitest to web workspace

--- packages/web/utils/temperature-unit.util.test.ts
+++ packages/web/utils/temperature-unit.util.test.ts
@@ -1,8 +1,13 @@
-import { describe, it, expect } from 'vitest';
-import {convertTemperature, TEMPERATURE_UNITS} from "#utils/temperature-unit.util.ts";
+import { describe, expect, it } from 'vitest';
+import {
+  convertTemperature,
+  TEMPERATURE_UNITS,
+} from '#utils/temperature-unit.util.ts';
 
 describe('Temperature unit.util', () => {
   it('converts fahrenheit to celsius', () => {
-    expect(convertTemperature({ celsius: 20, unit: TEMPERATURE_UNITS.FAHRENHEIT })).toEqual(68);
-  })
-})
+    expect(
+      convertTemperature({ celsius: 20, unit: TEMPERATURE_UNITS.FAHRENHEIT }),
+    ).toEqual(68);
+  });
+});
